fd14caeb694b2a291a44469b28892a1832bdcd0f
isimsek7 <isimsekmusic@gmail.com>
2024-08-08 00:22:18 +0300

    Add files via upload

---
+++ Base.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Inheritance
+{
+	public class BaseKisisi
+	{
+		public BaseKisisi()
+		{
+		}
+		public string _ad { get; set; }
+		public string _soyAd { get; set; }
+
+		public virtual void Ozet()
+		{
+			Console.WriteLine($"{_ad} {_soyAd}");
+		}
+
+
+	}
+	public class Ogrenci : BaseKisisi
+	{
+		public int _ogrenciNo { get; set; }
+
+		public override void Ozet()
+		{
+			Console.WriteLine($"{_ogrenciNo}'numarali {_ad} {_soyAd} ogrencisi.");
+		}
+	}
+	public class Ogretmen:BaseKisisi
+	{
+		public double _maasBilgisi { get; set; }
+
+        public override void Ozet()
+        {
+            Console.WriteLine($"{_ad} {_soyAd} adli ogretmenin maasi {_maasBilgisi} TL'dir.");
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Inheritance;
+Ogrenci ogrenci1 = new Ogrenci();
+ogrenci1._ad = "Murat";
+ogrenci1._soyAd = "Soyad";
+ogrenci1._ogrenciNo = 1234567789;
+
+Ogretmen ogretmen1 = new Ogretmen();
+ogretmen1._ad = "Kazim";
+ogretmen1._soyAd = "Nazim";
+ogretmen1._maasBilgisi = 27.000;
+
+ogrenci1.Ozet();
+ogretmen1.Ozet();
