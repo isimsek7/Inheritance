b66b6486d3139364ea4444f5276527ad967d5265
isimsek7 <isimsekmusic@gmail.com>
2024-08-08 15:57:13 +0300

    BaseKisisi.cs

--- Base.cs
+++ Base.cs
@@ -1,39 +1,33 @@
-﻿using System;
+using System;
 namespace Inheritance
 {
-	public class BaseKisisi
-	{
-		public BaseKisisi()
-		{
-		}
-		public string _ad { get; set; }
-		public string _soyAd { get; set; }
-
-		public virtual void Ozet()
-		{
-			Console.WriteLine($"{_ad} {_soyAd}");
-		}
-
-
-	}
-	public class Ogrenci : BaseKisisi
-	{
-		public int _ogrenciNo { get; set; }
-
-		public override void Ozet()
-		{
-			Console.WriteLine($"{_ogrenciNo}'numarali {_ad} {_soyAd} ogrencisi.");
-		}
-	}
-	public class Ogretmen:BaseKisisi
-	{
-		public double _maasBilgisi { get; set; }
-
-        public override void Ozet()
+    public class BaseKisisi
+    {
+        public BaseKisisi()
         {
-            Console.WriteLine($"{_ad} {_soyAd} adli ogretmenin maasi {_maasBilgisi} TL'dir.");
         }
+        public string _ad { get; set; }
+        public string _soyAd { get; set; }
+
+        public virtual void Ozet(string a,string b, int c)
+        {
+            Console.WriteLine($"{c}'numarali {a} {c} ogrencisi.");
+        }
+        public virtual void Ozet(double d, string a, string b)
+        {
+            Console.WriteLine($"{a} {b}' isimli ogretmenin maas bilgisi {d} TL'dir.");
+        }
+
+
+    }
+    public class Ogrenci : BaseKisisi
+    {
+        public int _ogrenciNo { get; set; }
+
+    }
+    public class Ogretmen : BaseKisisi
+    {
+        public double _maasBilgisi { get; set; }
     }
 }
-
 
